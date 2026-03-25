1894395eddcf5fba08531bc780cbb6b3b4f396e0
Rifqialba <rifqialba12@gmail.com>
2026-03-25 08:22:22 +0700

    feat: editing site const

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://albaporto.vercel.app",
-  integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
+  // site: "https://albaporto.vercel.app",
+  integrations: [mdx(), solidJs(), tailwind({ applyBaseStyles: false })],
 });
